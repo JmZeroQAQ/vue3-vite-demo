f84eda1879d8372cfb180649285fb6319f27b4b3
Yu Xian <1932699628@qq.com>
2023-07-14 21:40:31 +0800

    vue3 pinia的组合式写法

--- src/store/num.js
+++ src/store/num.js
@@ -1,36 +1,37 @@
 import { defineStore } from "pinia";
+import { ref, computed } from 'vue';
 
 // 第一个参数为id,用来做模块划分
-export const useNumStore = defineStore('numStore', {
+export const useNumStore = defineStore('numStore', () => {
     // 状态设定
-    state: () => {
-        return {
-            num: 10,
-        }
-    },
+    const num = ref(10);
     // 计算属性
-    getters: {
-        doubleNum() {
-            return this.num * 2;
-        },
-    },
+    const doubleNum = computed(() => num.value * 2);
     // 函数
-    actions: {
-        increaseNum() {
-            this.num++;
-        },
-        decreaseNum() {
-            this.num--;
-        },
-        asyncChangeNum() {
-            // 异步方法建议使用Promise包裹
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    this.num += 2;
-                    // 切换状态已经成功
-                    resolve();
-                }, 2000);
-            });
-        }
+    function increaseNum() {
+        num.value++;
     }
-})
+
+    function decreaseNum() {
+        num.value--;
+    }
+
+    async function asyncChangeNum() {
+        // 异步方法建议使用Promise包裹
+        return new Promise(resolve => {
+            setTimeout(() => {
+                num.value += 2;
+                // 切换状态已经成功
+                resolve();
+            }, 2000);
+        });
+    }
+
+    return {
+        num,
+        doubleNum,
+        increaseNum,
+        decreaseNum,
+        asyncChangeNum,
+    }
+});
